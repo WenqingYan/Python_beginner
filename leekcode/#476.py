f81cff422901b08524ce1316490199f94686af38
WenqingYan <32642110+WenqingYan@users.noreply.github.com>
2017-11-28 20:18:37 +0100

    Initial commit

---
+++ leekcode/#476.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'No.476'
+__author__ = 'wenqingy'
+
+class Solution(object):
+    def __init__(self, num):
+        self.num = num
+
+    def findComplement(self):
+        num = str(bin(self.num))
+        rev = ''
+        for n in range(len(num)):
+            if n > 1:
+                rev += str(int(num[n])^1)
+        return int(rev, base=2)
+
+#test
+ a = Solution(5)
+ a.findComplement()
+
+ b = Solution(1)
+ b.findComplement()
